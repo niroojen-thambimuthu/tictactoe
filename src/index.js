3d0bf8ea29500947f8d5bad79d6d65c90170a1a6
Niroojen Thambimuthu <niroojen1996@gmail.com>
2022-08-09 14:38:29 -0600

    update components
    update components

--- src/index.js
+++ src/index.js
@@ -2,6 +2,31 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css"
 
+const Square = () => {
+  return (
+    <div style={{
+      backgroundColor: "#202C37",
+      margin: 10,
+      padding: 20,
+    }}>
+      Square
+    </div>
+  );
+};
+
+const Board = () => {
+  return (
+    <div style={{
+      backgroundColor: "#283547",
+      margin: 10,
+      padding: 20,
+    }}>
+      Board
+      <Square />
+    </div>
+  );
+};
+
 const Game = () => {
   return (
     <div style={{
@@ -10,6 +35,7 @@ const Game = () => {
       padding: 20,
     }}>
       Game
+      <Board />
     </div>
   );
 };
