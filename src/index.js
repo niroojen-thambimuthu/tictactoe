19b74bae240cf3f26c33a7f2b1e9e249b40d8f8b
Niroojen Thambimuthu <niroojen1996@gmail.com>
2022-08-09 13:47:34 -0600

    File cleanup
    File cleanup

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const Game = () => {
+  return (
+    <div style={{
+      backgroundColor: "#202C37",
+      margin: 10,
+      padding: 20,
+    }}>
+      Game
+    </div>
+  );
+};
+
+ReactDOM.render(
+  <Game />,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
