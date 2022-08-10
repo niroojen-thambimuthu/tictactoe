0aa92ada9e05af2900fca4074a350e10c3ad253c
Niroojen Thambimuthu <niroojen1996@gmail.com>
2022-08-10 12:56:25 -0600

    Update index.js
    Playing with Props

--- src/index.js
+++ src/index.js
@@ -2,15 +2,22 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css"
 
-const Square = () => {
+const Square = (props) => {
   return (
     <div className="squareCanvas">
-      X
+      {props.value}
     </div>
   );
 };
 
 const Board = () => {
+
+  const squareRender = (temp) => {
+    return(
+      <Square value={temp} />
+    );
+  };
+
   return (
     <div style={{
       backgroundColor: "#283547",
@@ -19,19 +26,19 @@ const Board = () => {
     }}>
       Board
       <div className="boardCanvas">
-        <Square />
-        <Square />
-        <Square />
+        {squareRender(0)}
+        {squareRender(1)}
+        {squareRender(2)}
       </div>
       <div className="boardCanvas">
-        <Square />
-        <Square />
-        <Square />
+        {squareRender(3)}
+        {squareRender(4)}
+        {squareRender(5)}
       </div>
       <div className="boardCanvas">
-        <Square />
-        <Square />
-        <Square />
+        {squareRender(6)}
+        {squareRender(7)}
+        {squareRender(8)}
       </div>
     </div>
   );
