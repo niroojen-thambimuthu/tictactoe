557f9254c38164917bff0c6b2ba43893dd0d2d6f
Niroojen Thambimuthu <niroojen1996@gmail.com>
2022-08-13 16:40:56 -0600

    Update index.js
    Taking turns between X and O

--- src/index.js
+++ src/index.js
@@ -22,15 +22,17 @@ const Board = () => {
 
   const initialSquares = Array(9).fill(null);
   const [squares, setSquares] = useState(initialSquares);
+  const [xMove, setXMove] = useState(true);
 
   const clickEvent = (temp) => {
     // alert(`${temp} clicked`);
     // 1. make a copy of squares state array
     const newSquares = [...squares];
     // 2. mutate copy, setting the i-th element to 'X'
-    newSquares[temp] = 'X';
+    newSquares[temp] = xMove ? "X":"O";
     // 3. Call the setSquare function with the mutated copy
     setSquares(newSquares);
+    setXMove(!xMove);
   }
 
   const squareRender = (temp) => {
@@ -39,13 +41,16 @@ const Board = () => {
     );
   };
 
+  const status = `Next Player: ${xMove ? "X":"O"}`;
+
   return (
     <div style={{
       backgroundColor: "#283547",
       margin: 10,
       padding: 20,
     }}>
-      Board
+      {/* Board */}
+      <div>{status}</div>
       <div className="boardCanvas">
         {squareRender(0)}
         {squareRender(1)}
